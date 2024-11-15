baae4421cc94db5c5fbfad3b913bf0a9867d8270
Miguel Dev <saenzm963@gmail.com>
2024-11-15 10:18:14 -0500

    Archivo suma creado

---
+++ src/Suma.java
@@ -0,0 +1,10 @@
+public class Suma {
+    public static void main(String[] args) {
+        System.out.println(suma(10, 20));
+    }
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+}
